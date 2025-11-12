0d59fcf8b90ffb3e8f8f9f73dbb4246f0971989c
AnnPue <hurui200612@outlook.com>
2025-11-12 22:14:50 +0800

    test

---
+++ 01_Hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "你好吗 世界\n" << endl;
+    putchar('\a');
+    int n =10;
+    for (int i = 0; i < n; i ++){
+        cout << i << endl;
+        cout << 12;
+        cout << "i love love you" << endl;
+    }
+    return 0;
+}
