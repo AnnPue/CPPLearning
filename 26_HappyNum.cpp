6b9ce1e89a6d4703bc86c2b080e38fa417e45701
AnnPue <hurui200612@outlook.com>
2025-11-25 19:26:44 +0800

    add comments

--- 26_HappyNum.cpp
+++ 26_HappyNum.cpp
@@ -1,11 +1,16 @@
+/*
+ 给定一个正整数，请你判断这个数是不是快乐数。
+快乐数：对于一个正整数，每次把他替换为他每个位置上的数字的平方和，
+        如果这个数能变为 1 则是快乐数，如果不可能变成 1 则不是快乐数。 
+*/
 class Solution {
 public:
     bool happynum(int n) {
         if (n == 1) return true;
-        else if(n < 10)return false;
+        else if (n < 10) return false;
         int i = 0;
         int sum = 0;
-        while(n != 0){
+        while (n != 0){
             i = n % 10;
             sum += i * i;
             n /= 10;
