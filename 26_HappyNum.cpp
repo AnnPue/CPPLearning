8d7667817aa61907d1e33b5e5330d5ae05cc0175
AnnPue <hurui200612@outlook.com>
2025-11-26 11:00:27 +0800

    feat: complete happynum

--- 26_HappyNum.cpp
+++ 26_HappyNum.cpp
@@ -3,6 +3,9 @@
 快乐数：对于一个正整数，每次把他替换为他每个位置上的数字的平方和，
         如果这个数能变为 1 则是快乐数，如果不可能变成 1 则不是快乐数。 
 */
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     bool happynum(int n) {
@@ -18,3 +21,12 @@ public:
         return happynum(sum);
     }
 };
+
+int main() {
+    Solution s;
+    int n;
+    cin >> n;
+    if (s.happynum(n)) cout << "true" << endl;
+    else cout << "false" << endl;
+    return 0;
+}
