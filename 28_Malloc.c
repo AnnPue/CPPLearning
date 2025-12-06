27c1b9e88bcdc9708f484d3bf14d213590ba0904
AnnPue <hurui200612@outlook.com>
2025-12-06 17:19:26 +0800

    refactor: improve string handling and memory allocation in malloc example

--- 28_Malloc.c
+++ 28_Malloc.c
@@ -5,13 +5,20 @@
 
 int main()
 {   
-    char *r = fgets(r, 100, stdin);
-    size_t n = strlen(r);
-    char *t = (char*)malloc(n * sizeof(char) + 1);
-    strcpy(t, r);
+    char r[100];
+    printf("r: ");
+    fgets(r, 100, stdin);
+    printf("r: %s", r);
 
-    printf("r: %s\n", r);
-    printf("t: %s\n", t);
-    free(t);
+    char *s = r;
+    printf("s: %s", s);
+
+    size_t n = strlen(r);
+    char *t = (char*)malloc((n + 1) * sizeof(char)); // 给t分配空间，长度为n+1（包括结尾\0）
+    
+    r[0] = toupper(r[0]);//将r的第一个字符转换为大写字母
+    strcpy(t, r);//将r里面的字符串复制到t里面
+    printf("t: %s", t);
+    free(t);//释放t
     return 0;
 }
