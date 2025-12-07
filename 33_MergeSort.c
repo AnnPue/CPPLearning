d39672ac5db9fcc94e7172cae97ff965dee6f8e6
AnnPue <hurui200612@outlook.com>
2025-12-07 18:06:35 +0800

    feat: implement merge sort algorithm

---
+++ 33_MergeSort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>// for malloc/free
+
+int min(int, int);
+void merge_sort(int [], int);
+int main(void) {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    merge_sort(arr, len);
+    for (int i = 0; i < len; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+int min(int x, int y) {
+    return x < y ? x : y;
+}
+void merge_sort(int arr[], int len) {
+    int *a = arr;
+    int *b = (int *) malloc(len * sizeof(int));
+    int seg, start;
+    for (seg = 1; seg < len; seg += seg) {
+        for (start = 0; start < len; start += seg * 2) {
+            int low = start, mid = min(start + seg, len), high = min(start + seg * 2, len);
+            int k = low;
+            int start1 = low, end1 = mid;
+            int start2 = mid, end2 = high;
+            while (start1 < end1 && start2 < end2)
+                b[k++] = a[start1] < a[start2] ? a[start1++] : a[start2++];
+            while (start1 < end1)
+                b[k++] = a[start1++];
+            while (start2 < end2)
+                b[k++] = a[start2++];
+        }
+        int *temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a != arr) {
+        int i;
+        for (i = 0; i < len; i++)
+            b[i] = a[i];
+        b = a;
+    }
+    free(b);
+}
