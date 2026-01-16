a9ea0bdb01377db8cc5d4884c2e8ddc3926cd4cb
AnnPue <hurui200612@outlook.com>
2026-01-16 19:06:29 +0800

    add space

--- 03_Season.cpp
+++ 03_Season.cpp
@@ -5,10 +5,10 @@ int main() {
     int date,month;
     cin >> date;
     month = date%100;
-    if (month<6&&month>2)cout<<"spring";
-    else if (month<9&&month>5)cout<<"summer";
-    else if (month<12&&month>8)cout<<"autumn";
-    else cout<<"winter";
+    if (month<6&&month>2)cout<<"spring ";
+    else if (month<9&&month>5)cout<<"summer ";
+    else if (month<12&&month>8)cout<<"autumn ";
+    else cout<<"winter ";
 //或者:
     switch(month%12/3)
     {
