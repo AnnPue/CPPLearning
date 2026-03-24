c91a5ee034c106d3c64cef5127822f62b11df59d
AnnPue <hurui200612@outlook.com>
2026-03-24 16:14:52 +0800

    simplify

--- 23_EuclidAlgo.cpp
+++ 23_EuclidAlgo.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a, int b){
-    if (b == 0) return a;
-    int r = a % b;
-    return gcd(b, r);
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
 
-int main(){
+int main()
+{
     int a, b;
     cin >> a >> b;
     cout << abs(gcd(a, b)) << endl;
