7322e7bf7b9149b78cc2c31d8e4307b4de0314ca
AnnPue <hurui200612@outlook.com>
2026-05-11 10:38:51 +0800

    feat: improve code readability and consistency in Hanoi function

--- 12_Hanoi.cpp
+++ 12_Hanoi.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void Hanoi(int n, char from, char assist, char to){
-    if (n == 1){
+// n: number of disks
+// from: the source pole
+// assist: the auxiliary pole
+// to: the destination pole
+void Hanoi(int n, char from, char assist, char to)
+{
+    if (n == 1)
+    {
         printf("move %d from %c to %c\n", n, from, to);
         return;
     }
     Hanoi(n - 1, from, to, assist);
     printf("move %d from %c to %c\n", n, from, to);
     Hanoi(n - 1, assist, from, to);
-
 }
 
-int main(){
-    cout << "请输入汉诺塔层数" << endl;
+int main()
+{
     int n;
+    cout << "Enter the number of disks: \n";
     cin >> n;
-    Hanoi(n, 'l', 'm', 'r');
+    // L: left, M: middle, R: right
+    Hanoi(n, 'L', 'M', 'R');
     return 0;
 }
